0a181157e77b01115c0aa56554331bb1b52fd5bc
kawarag1 <dima.kretinin.05@mail.ru>
2025-11-14 22:47:43 +0700

    fixed alembic config and add new vericon of db

--- alembic/env.py
+++ alembic/env.py
@@ -6,11 +6,12 @@ from sqlalchemy import pool
 from alembic import context
 
 from orm.models.models import Base
+from settings.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("sqlalchemy.url", f"{settings.db_url}?async_fallback=True")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata()
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
