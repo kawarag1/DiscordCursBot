a4312b51e9f73d27d7ed187544aa6b50ea16c238
kawaragi <dima.kretinin.05@mail.ru>
2025-11-17 12:30:50 +0700

    change and get funcs for server profile

--- app/src/services/server_profile_service.py
+++ app/src/services/server_profile_service.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.src.orm.database.repo.user_server_repo import UserServerRepository
 from app.src.schemas.request.server_user_schema import ServerUserCreate
@@ -11,3 +13,10 @@ class ServerProfileService():
 
     async def delete_server_profile(self, ds_id: int):
         await UserServerRepository(self.session).delete_by_DSid(ds_id)
+
+    async def change_server_profile(self, user: ServerUserCreate):
+        await UserServerRepository(self.session).update_by_ds_id(user.ds_id, **user.model_dump())
+
+    async def get_server_profile(self, ds_id: int):
+        return await UserServerRepository(self.session).get_by_ds_id(ds_id)
+        
