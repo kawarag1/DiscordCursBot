b4542f1a090d23c3686785e113e742a4b8a4edd4
kawarag1 <dima.kretinin.05@mail.ru>
2025-11-21 15:35:05 +0700

    added slash command to create voice_chat with user_limit and added auto deleting channel when it is empty

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ token = settings.TOKEN
 
 print(token)
 
-
+auto_delete_channels = set()
 
 bot = commands.Bot(
     command_prefix = "!",
@@ -168,5 +168,38 @@ async def check_level(inter: disnake.ApplicationCommandInteraction):
                 await inter.response.send_message(f"Ваш уровень: {profile.level}")
     
 
+@bot.slash_command(name="create_voice", description="Создать голосовой канал")
+async def create_voice(inter: disnake.ApplicationCommandInteraction,
+    name: str = commands.Param(description="Название канала"),
+    user_limit: int = commands.Param(0, description="Лимит пользователей (0 = без лимита)")):
+    try:
+        voice_channel = await inter.guild.create_voice_channel(
+            name = name,
+            user_limit = user_limit,
+            category = inter.channel.category,
+            reason = f"Создано пользователем {inter.author}"
+        )
+
+        auto_delete_channels.add(voice_channel.id)
+
+        await inter.response.send_message(
+            f"Голосовой канал {voice_channel.mention} создан",
+            ephemeral = True
+        )
+    except Exception as e:
+        await inter.response.send_message(f"❌ Ошибка: {e}", ephemeral=True)
+
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+     if before.channel and before.channel.id in auto_delete_channels:
+         if len(before.channel.members) == 0:
+            try:
+                await before.channel.delete(reason="Автоматическое удаление пустого канала")
+                auto_delete_channels.discard(before.channel.id)
+                print(f"🗑️ Удалён пустой канал: {before.channel.name}")
+            except Exception as e:
+                print(f"❌ Ошибка при удалении канала: {e}")
 
 bot.run(token)
