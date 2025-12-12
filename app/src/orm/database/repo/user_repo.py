b0130addeedc65950507e28068419b8d95e21b14
kawarag1 <dima.kretinin.05@mail.ru>
2025-12-13 05:01:05 +0700

    added get_by_ds_id

--- app/src/orm/database/repo/user_repo.py
+++ app/src/orm/database/repo/user_repo.py
@@ -1,6 +1,24 @@
+from sqlalchemy import select
 from app.src.orm.database.repo.abc_repo import AbstractRepository
 from app.src.orm.models.models import User
+from app.src.schemas.request.user_schema import UserCreate
 
 
 class UserRepository(AbstractRepository):
     model = User
+
+    async def get_by_ds_id(self, ds_id: int):
+        query = select(self.model).where(self.model.ds_id == ds_id)
+        result_ = await self._session.execute(query)
+        result =  result_.scalars().first()
+        user = UserCreate(
+            ds_id = result.ds_id,
+            avatar_url = result.avatar_url if result.avatar_url else "",
+            created_at = result.created_at,
+            nickname = result.nickname,
+            message_count = result.message_count,
+            level = result.level,
+            guild_id = result.guild_id
+        )
+        return user
+        
