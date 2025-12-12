9c0ae2b92d8dc02665ce4c5e2339944e661196ac
kawarag1 <dima.kretinin.05@mail.ru>
2025-12-13 05:00:11 +0700

    added new service for guilds

---
+++ app/src/services/guild_service.py
@@ -0,0 +1,25 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from disnake import Guild
+
+from app.src.orm.models.models import Guild as ModelGuild
+from app.src.schemas.request.guild_schema import GuildSchema
+
+class GuildService():
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def check_guild_by_id(self, guild_id):
+        query = select(ModelGuild).filter(ModelGuild.id == guild_id)
+        result = await self.session.execute(query)
+        guild = result.first()
+        if guild is not None:
+            return True
+    
+    async def add_new_guild(self, guild: Guild):
+        new_guild = ModelGuild(
+            id = guild.id,
+            name = guild.name
+        )
+        self.session.add(new_guild)
+        await self.session.commit()
