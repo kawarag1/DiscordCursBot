4b6f480e96016cdbed211e8292012a53b84217a5
kawarag1 <dima.kretinin.05@mail.ru>
2025-12-13 05:00:35 +0700

    move func from server_profile to user_service

--- app/src/services/user_service.py
+++ app/src/services/user_service.py
@@ -12,3 +12,9 @@ class UserService():
 
     async def delete_user(self, ds_id: int):
         await UserRepository(self.session).delete_by_DSid(ds_id)
+
+    async def get_server_profile(self, ds_id: int):
+        await UserRepository(self.session).get_by_ds_id(ds_id)
+
+    async def change_server_profile(self, user: UserCreate):
+        await UserRepository(self.session).update_by_ds_id(user.ds_id, **user.model_dump())
