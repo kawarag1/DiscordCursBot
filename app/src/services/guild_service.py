a08c070bf690df1a0af3421b8aa73a66437fe494
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-17 15:37:15 +0700

    теперь фиксируется владелец сервера

--- app/src/services/guild_service.py
+++ app/src/services/guild_service.py
@@ -8,17 +8,19 @@ class GuildService():
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def check_guild_by_id(self, guild_id):
+    async def check_guild_by_id(self, guild_id: int):
         query = select(ModelGuild).filter(ModelGuild.id == guild_id)
         result = await self.session.execute(query)
         guild = result.first()
         if guild is not None:
             return True
+        return False
     
     async def add_new_guild(self, guild: disnake.Guild):
         new_guild = ModelGuild(
             id = guild.id,
-            name = guild.name
+            name = guild.name,
+            owner_id = guild.owner_id,
         )
         self.session.add(new_guild)
         await self.session.commit()
