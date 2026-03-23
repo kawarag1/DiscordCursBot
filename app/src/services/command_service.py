5f290a4902f92a24dfa449763b81183311abe21a
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-24 00:09:59 +0700

    функция для проверки команды на работоспособность из постгри

--- app/src/services/command_service.py
+++ app/src/services/command_service.py
@@ -1,10 +1,13 @@
 
-from sqlalchemy import delete, select
+from fastapi import Depends
+from sqlalchemy import delete, exists, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.schemas.request.disable_command_schema import DisableCommandSchema
 from app.src.orm.database.repo.command_repo import CommandRepository
 from app.src.orm.models.models import DisabledCommands as ModelCommand
+from app.src.utils.redis.redis_client import AsyncRedisClient, get_redis
+from app.src.settings.settings import settings
 
 class CommandService:
     def __init__(self, session: AsyncSession):
@@ -29,4 +32,13 @@ class CommandService:
         await self.session.execute(delete_query)
         self.session.commit()
         return result.scalars().first()
+    
+    async def check_command(self, command: DisableCommandSchema):
+        query = select(exists().where(
+            ModelCommand.guild_id == command.guild_id,
+            ModelCommand.command_name == command.command_name
+        ))
+        result = await self.session.execute(query)
+        
+        return not result.scalar()
     
