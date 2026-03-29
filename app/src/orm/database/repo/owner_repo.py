c60463f47d0c724add4375ed76b6e6e8e574e6bf
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-29 23:40:36 +0700

    new funcs

--- app/src/orm/database/repo/owner_repo.py
+++ app/src/orm/database/repo/owner_repo.py
@@ -1,5 +1,32 @@
+from select import select
+from sqlalchemy import exists, update
+
 from app.src.orm.database.repo.abc_repo import AbstractRepository
 from app.src.orm.models.models import Owner
+from app.src.schemas.response.owner_schema import OwnerSchema
 
 class OwnerRepository(AbstractRepository):
     model = Owner
+
+    async def get_by_ds_id(self, ds_id: int):
+        query = select(self.model).where(self.model.ds_id == ds_id)
+        result_ = await self._session.execute(query)
+        result = result_.scalars().first()
+        if result:
+            return OwnerSchema(
+                id=result.id,
+                ds_id=result.ds_id,
+                email=result.email if result.email else "",
+                refresh_token=result.refresh_token
+            )
+        return None
+
+    async def exists_by_ds_id(self, ds_id: int) -> bool:
+        query = select(exists(select(self.model).where(self.model.ds_id == ds_id)))
+        result = await self._session.execute(query)
+        return result.scalar()
+
+    async def update_refresh_token(self, ds_id: int, refresh_token: str):
+        query = update(self.model).where(self.model.ds_id == ds_id).values(refresh_token=refresh_token)
+        await self._session.execute(query)
+        await self.commit()
