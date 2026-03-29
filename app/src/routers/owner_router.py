5484eba28abaaaa8bdcacf4ae504062c5563ceaf
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-29 20:00:32 +0700

    hotfix

--- app/src/routers/owner_router.py
+++ app/src/routers/owner_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.params import Body
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.database.database import get_session
@@ -9,7 +10,7 @@ router = APIRouter(prefix="/auth", tags=["Авторизация"])
 
 
 @router.post("/get_owner", description="Авторизация владельца", response_model=OwnerSchema)
-async def exchange_code(code: str, session: AsyncSession = Depends(get_session)):
+async def exchange_code(code: str = Body(...), session: AsyncSession = Depends(get_session)):
     owner_id = await OwnerService(session).exchange_code(code=code)
     owner_info = await OwnerService(session).get_owner_info(owner_id.access_token)
     return await OwnerService(session).add_owner(owner_info.id)
