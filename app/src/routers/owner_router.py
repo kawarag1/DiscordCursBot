5b56c745e69efc04d968b472b583dc9b274c2c80
kawarag1 <dima.kretinin.05@mail.ru>
2026-04-09 01:45:56 +0700

    added refresh_access ds token endpoint and endpoint for gettings owner guilds

--- app/src/routers/owner_router.py
+++ app/src/routers/owner_router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Request, Response
 from fastapi.params import Body
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.database.database import get_session
 from app.src.schemas.request.code import CodeRequest
 from app.src.schemas.response.owner_schema import OwnerSchema
+from app.src.security.session_auth_token import session_auth
+from app.src.security.oauth import get_current_owner
 from app.src.services.owner_service import OwnerService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация"])
@@ -22,3 +24,12 @@ async def exchange_code(code: CodeRequest, response: Response, session: AsyncSes
         samesite="lax",
         max_age=owner_token.expires_in)
     return await OwnerService(session).add_owner(ds_id, owner_token.access_token, owner_token.refresh_token, owner_token.session_token, owner_token.expires_at)
+
+
+@router.post("/refresh_session_token", dependencies=[Depends(session_auth)],description="Обновление сессионного токена", response_model=OwnerSchema)
+async def refresh_session_token(owner: OwnerSchema = Depends(get_current_owner), session: AsyncSession = Depends(get_session)):
+    return await OwnerService(session).refresh_session_token(session_token=owner.session_token)
+
+@router.get("/guilds", dependencies=[Depends(session_auth)], description="Получение списка серверов владельца")
+async def get_guilds(owner: OwnerSchema = Depends(get_current_owner), session: AsyncSession = Depends(get_session)):
+    return await OwnerService(session).get_user_guilds(owner=owner)
