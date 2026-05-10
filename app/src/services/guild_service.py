17c1d1e96ee6721bc68752da364d029cbc2002dc
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-10 14:51:36 +0700

    hotfix

--- app/src/services/guild_service.py
+++ app/src/services/guild_service.py
@@ -78,11 +78,11 @@ class GuildService():
             
             return response.json()
         
-    async def get_users_on_guilds(self, guild_id: int):
+    async def get_users_on_guilds(self, owner: OwnerSchema, guild_id: int):
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 settings.GUILD_MEMBERS_URI.format(guild_id=guild_id),
-                headers={"Authorization": f"Bot {settings.BOT_TOKEN}"}
+                headers={"Authorization": f"Bot {owner.access_token}"}
             )
 
             if response.status_code != 200:
@@ -109,7 +109,7 @@ class GuildService():
                             if guild.get("icon")
                             else None
                         ),
-                        approximate_member_count=await self.get_users_on_guilds(int(guild["id"]))
+                        approximate_member_count=await self.get_users_on_guilds(owner, int(guild["id"]))
                     )
                 )
             
