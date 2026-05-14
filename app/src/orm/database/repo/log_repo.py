57441ff15055e79e812df735f7e12e986bb85f3e
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-15 04:20:21 +0700

    hotfix

--- app/src/orm/database/repo/log_repo.py
+++ app/src/orm/database/repo/log_repo.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from app.src.orm.database.repo.abc_repo import AbstractRepository
 from app.src.orm.models.models import Log_entries
 
@@ -5,6 +7,6 @@ class LogRepository(AbstractRepository):
     model = Log_entries
 
     async def get_by_guild_id(self, guild_id: int):
-        query = self._session.select(self.model).filter_by(guild_id=guild_id)
+        query = select(self.model).filter_by(guild_id=guild_id)
         result = await self._session.execute(query)
         return result.scalars().all()
