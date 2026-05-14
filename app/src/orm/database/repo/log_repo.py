7cc2e74491fe6bf4e98dfb8d458ec6d0d3dbb0f9
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-15 03:53:06 +0700

    added func to get actions by ds id

--- app/src/orm/database/repo/log_repo.py
+++ app/src/orm/database/repo/log_repo.py
@@ -3,3 +3,8 @@ from app.src.orm.models.models import Log_entries
 
 class LogRepository(AbstractRepository):
     model = Log_entries
+
+    async def get_by_guild_id(self, guild_id: int):
+        query = self._session.query(self.model).filter_by(guild_id=guild_id)
+        result = await self._session.execute(query)
+        return result.scalars().all()
