8dc75c7c2eee28356ceed49a8260e795e8e36c7d
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-15 04:09:52 +0700

    action router

--- app/src/routers/main_router.py
+++ app/src/routers/main_router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.src.routers.command_router import router as command_router
 from app.src.routers.owner_router import router as owner_router
 from app.src.routers.guilds_router import router as guilds_router
+from app.src.routers.action_router import router as action_router
 
 
 router = APIRouter(
@@ -12,3 +13,4 @@ router = APIRouter(
 router.include_router(command_router)
 router.include_router(owner_router)
 router.include_router(guilds_router)
+router.include_router(action_router)
