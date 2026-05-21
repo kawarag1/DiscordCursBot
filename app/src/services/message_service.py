c818826425c567d102b61f42bb4491a301c48cfb
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-21 18:04:22 +0700

    small fix

--- app/src/services/message_service.py
+++ app/src/services/message_service.py
@@ -3,15 +3,18 @@ import disnake
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.models.models import Messages as ModelMessage, Attachments as ModelAttachment
+from app.src.orm.database.repo.user_repo import UserRepository
 
 class MessageService():
     def __init__(self, session: AsyncSession):
         self.session = session
 
     async def add_message(self, message: disnake.Message):
+        ds_id = await UserRepository.get_userID_by_dsID(message.author.id)
+
         message_ = ModelMessage(
             id = message.id,
-            user_id = message.author.id,
+            user_id = message.ds_id,
             guild_id = message.guild.id,
             content = message.content,
             created_at = message.created_at
