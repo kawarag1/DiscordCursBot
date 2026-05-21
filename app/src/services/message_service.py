85f5ca45329b8cc1618b9d6891a67e6a078d3590
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-21 18:09:35 +0700

    hotfix

--- app/src/services/message_service.py
+++ app/src/services/message_service.py
@@ -8,13 +8,14 @@ from app.src.orm.database.repo.user_repo import UserRepository
 class MessageService():
     def __init__(self, session: AsyncSession):
         self.session = session
+        self.user_repo = UserRepository(session)
 
     async def add_message(self, message: disnake.Message):
-        ds_id = await UserRepository.get_userID_by_dsID(message.author.id)
+        id = await self.user_repo.get_userID_by_dsID(message.author.id)
 
         message_ = ModelMessage(
             id = message.id,
-            user_id = message.ds_id,
+            user_id = id,
             guild_id = message.guild.id,
             content = message.content,
             created_at = message.created_at
