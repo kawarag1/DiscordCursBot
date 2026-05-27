6be1c0343681f484a1ec2529af9aa00d6aa46913
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-28 01:49:47 +0700

    hotfix

--- app/src/schemas/response/owner_schema.py
+++ app/src/schemas/response/owner_schema.py
@@ -1,8 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class OwnerSchema(BaseModel):
     id: int
     ds_id: int
     email: Optional[str]
+
+    model_config = ConfigDict(from_attributes=True)
