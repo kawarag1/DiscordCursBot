a532a4d12ef86cb95ba55325fc55d2eb58ac5560
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-28 00:50:34 +0700

    hotfix\

--- app/src/orm/database/repo/owner_repo.py
+++ app/src/orm/database/repo/owner_repo.py
@@ -27,30 +27,4 @@ class OwnerRepository(AbstractRepository):
     async def exists_by_ds_id(self, ds_id: int) -> bool:
         query = select(exists(select(self.model).where(self.model.ds_id == ds_id)))
         result = await self._session.execute(query)
-        return result.scalar()
-
-    async def update_refresh_token(self, ds_id: int, access_token: str, refresh_token: str, session_token: str, expires_at: int | datetime):
-        query = update(self.model).where(self.model.ds_id == ds_id).values(
-            access_token=access_token,
-            refresh_token=refresh_token,
-            session_token=session_token,
-            expires_at=expires_at
-        )
-        await self._session.execute(query)
-        await self.commit()
-
-    async def get_by_session_token(self, session_token: str):
-        query = select(self.model).where(self.model.session_token == session_token)
-        result_ = await self._session.execute(query)
-        result = result_.scalars().first()
-        if result:
-            return OwnerSchema(
-                id=result.id,
-                ds_id=result.ds_id,
-                email=result.email if result.email else "",
-                access_token=result.access_token if result.access_token else "",
-                refresh_token=result.refresh_token if result.refresh_token else "",
-                session_token=result.session_token if result.session_token else "",
-                expires_at=int(result.expires_at.timestamp()) if result.expires_at else None
-            )
-        return None
+        return result.scalar()    
