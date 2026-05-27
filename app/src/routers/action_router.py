19bea1ae61f5347fcaaf6bd5d25b28fc3c64bc7b
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-28 00:53:02 +0700

    убрал старую схему авторизации

--- app/src/routers/action_router.py
+++ app/src/routers/action_router.py
@@ -3,13 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.database.database import get_session
 from app.src.schemas.response.action_schema import ActionSchema
-from app.src.security.session_auth_token import session_auth
 from app.src.security.oauth import get_current_owner
 from app.src.services.action_service import ActionService
 
 
 router = APIRouter(prefix="/actions", tags=["Действия на серверах"])
 
-@router.get("/{guild_id}", dependencies=[Depends(session_auth)], description="Получение списка действий на сервере", response_model=list[ActionSchema])
+@router.get("/{guild_id}", description="Получение списка действий на сервере", response_model=list[ActionSchema])
 async def get_actions(guild_id: str, owner = Depends(get_current_owner), session: AsyncSession = Depends(get_session)):
     return await ActionService(session).get_actions(int(guild_id))
