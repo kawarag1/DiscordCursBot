df56c2786be55cd5ecd3a2839d0ed26c6aa46615
kawarag1 <dima.kretinin.05@mail.ru>
2026-05-28 02:26:54 +0700

    renaming

--- app/src/utils/YM/__init__.py
+++
@@ -1,6 +0,0 @@
-from yandex_music import ClientAsync
-from app.src.settings.settings import settings
-import asyncio
-
-client = asyncio.run(ClientAsync(settings.YM_TOKEN).init())
-client.request.set_timeout(30)
