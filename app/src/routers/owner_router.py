25968170bc761df06dbe9bb64e40833e3a9fb153
kawaragi <dima.kretinin.05@mail.ru>
2026-05-30 03:06:56 +0700

    small fix

--- app/src/routers/owner_router.py
+++ app/src/routers/owner_router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends, Response
-import redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.database.database import get_session
 from app.src.schemas.request.code import CodeRequest
 from app.src.schemas.response.access_token import AccessToken
 from app.src.schemas.response.owner_schema import OwnerSchema
-from app.src.security.oauth import get_current_owner
+from app.src.security.oauth import get_current_owner, get_current_owner_from_refresh
 from app.src.services.owner_service import OwnerService
 from app.src.utils.redis.redis_client import get_redis
 from app.src.settings.settings import settings
@@ -35,7 +34,8 @@ async def get_current_owner_info(owner: OwnerSchema = Depends(get_current_owner)
     return owner
 
 @router.post("/refresh", description="Обновление токенов", response_model=AccessToken)
-async def refresh_tokens(response: Response, owner: OwnerSchema = Depends(get_current_owner), session: AsyncSession = Depends(get_session), redis = Depends(get_redis(0))):
+async def refresh_tokens(response: Response, owner: OwnerSchema = Depends(get_current_owner_from_refresh), session: AsyncSession = Depends(get_session), redis = Depends(get_redis(0))):
     tokens = await OwnerService(session, redis).refresh_tokens(owner.id)
     response.set_cookie(key="access_token", value=tokens.access_token, httponly=True, max_age=settings.JWT_ACCESS_TOKEN_LIFETIME_MINUTES * 60)
     response.set_cookie(key="refresh_token", value=tokens.refresh_token, httponly=True, max_age=settings.JWT_REFRESH_TOKEN_LIFETIME_DAYS * 24 * 60 * 60)
+    return tokens
