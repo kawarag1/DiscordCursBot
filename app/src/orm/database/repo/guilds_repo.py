0d8dd2b64f6e4f895aecd3d339c497824414f04d
kawarag1 <dima.kretinin.05@mail.ru>
2026-06-04 15:56:31 +0700

    methods for welcome message

--- app/src/orm/database/repo/guilds_repo.py
+++ app/src/orm/database/repo/guilds_repo.py
@@ -2,6 +2,7 @@ from sqlalchemy import delete, select
 
 from app.src.orm.database.repo.abc_repo import AbstractRepository
 from app.src.orm.models.models import Attachments, Guild, Messages
+from app.src.schemas.request.welcome_message_schema import WelcomeMessageSchema
 
 class GuildsRepository(AbstractRepository):
     model = Guild
@@ -13,3 +14,17 @@ class GuildsRepository(AbstractRepository):
     async def delete_messages(self, guild_id: int):
         query = delete(Messages).where(Messages.guild_id == guild_id)
         await self._session.execute(query)
+    
+    async def get_welcome_message(self, guild_id: int) -> str | None:
+        query = select(Guild.welcome_message).where(Guild.id == guild_id)
+        result = await self._session.execute(query)
+        welcome_message = result.scalar_one_or_none()
+        return welcome_message
+
+    async def update_welcome_message(self, guild_id: str, welcome_message: WelcomeMessageSchema):
+        query = select(Guild).where(Guild.id == int(guild_id))
+        result = await self._session.execute(query)
+        guild = result.scalar_one_or_none()
+        if guild:
+            guild.welcome_message = welcome_message.welcome_message
+            await self._session.commit()
