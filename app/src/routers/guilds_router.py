81969d677337b3151bc81e67493b0de83ebb005b
kawarag1 <dima.kretinin.05@mail.ru>
2026-06-04 15:57:43 +0700

    put endpoint to set custom welcome message

--- app/src/routers/guilds_router.py
+++ app/src/routers/guilds_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.orm.database.database import get_session
+from app.src.schemas.request.welcome_message_schema import WelcomeMessageSchema
 from app.src.schemas.response.guild_schema import GuildSchema
 from app.src.schemas.response.member_schema import MemberSchema
 from app.src.schemas.response.owner_schema import OwnerSchema
@@ -34,3 +35,8 @@ async def ban_member(guild_id: str, user_id: str, ban_data: BanSchema, owner: Ow
 @router.delete("/{guild_id}/members/{user_id}", description="Исключить участника с сервера")
 async def kick_member(guild_id: str, user_id: str, ban_data: BanSchema, owner: OwnerSchema = Depends(get_current_owner), session: AsyncSession = Depends(get_session)):
     await GuildService(session).kick_member(owner.ds_id, int(guild_id), int(user_id), ban_data.reason)
+
+@router.put("/{guild_id}/welcome-message", description="Установка приветственного сообщения для сервера")
+async def update_welcome_message(guild_id: str, welcome_message: WelcomeMessageSchema, owner: OwnerSchema = Depends(get_current_owner), session: AsyncSession = Depends(get_session)):
+    welcome_message.guild_id = int(guild_id)
+    await GuildService(session).update_welcome_message(guild_id, welcome_message)
