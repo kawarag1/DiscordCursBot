5ea4aec0fb33d0ed1de58751831a6a7837b6e8e5
kawarag1 <dima.kretinin.05@mail.ru>
2026-06-19 00:44:13 +0700

    я ебаснат

--- app/src/services/action_service.py
+++ app/src/services/action_service.py
@@ -75,6 +75,7 @@ class ActionService:
     async def clear_actions(self, guild_id: int):
         delete_query = delete(Log_entries).filter(Log_entries.guild_id == guild_id)
         await self.session.execute(delete_query)
+        await self.session.commit()
     
 
     
